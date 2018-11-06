efe74d27e549ca0d745250c66702d2820b860404
David Currie <dcurrie@cloudbees.com>
2018-11-06 15:28:58 +0000

    Test 2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Test Jenkins X golang http example"
+	title := "Test 2 Jenkins X golang http example"
 
 	from := ""
 	if r.URL != nil {
